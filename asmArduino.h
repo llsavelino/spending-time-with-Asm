573af78e8523c7fe2fbc63b284b03d0725c7ee6b
Lenilson A. Santos <savelino852@gmail.com>
2025-05-06 21:17:15 -0300

    Update asmArduino.h

--- asmArduino.h
+++ asmArduino.h
@@ -1,13 +1,17 @@
 #pragma once
-extern "C" { void main__(void); void loop__(void); };
-extern volatile uint8_t *portb_reg;
-extern volatile uint8_t *ddrb_reg;
-extern volatile uint8_t *pinb_reg;
+extern "C" 
+{ 
+  void main__               (void);
+  void loop__               (void); 
+};
+extern volatile uint8_t* portb_reg;
+extern volatile uint8_t*  ddrb_reg;
+extern volatile uint8_t*  pinb_reg;
 
-extern volatile uint8_t *portc_reg;
-extern volatile uint8_t *ddrc_reg;
-extern volatile uint8_t *pinc_reg;
+extern volatile uint8_t* portc_reg;
+extern volatile uint8_t*  ddrc_reg;
+extern volatile uint8_t*  pinc_reg;
 
-extern volatile uint8_t *portd_reg;
-extern volatile uint8_t *ddrd_reg;
-extern volatile uint8_t *pind_reg;
+extern volatile uint8_t* portd_reg;
+extern volatile uint8_t*  ddrd_reg;
+extern volatile uint8_t*  pind_reg;
