0f89de2eee4676197ed3a1e0c46b6c385487c7cd
Lenilson A. Santos <savelino852@gmail.com>
2025-05-06 21:04:47 -0300

    Add files via upload

---
+++ specialRegisters.C
@@ -0,0 +1,12 @@
+volatile uint8_t 
+  *portb_reg  = (volatile uint8_t *)0x25, // PORTB
+  *ddrb_reg   = (volatile uint8_t *)0x24, // DDRB
+  *pinb_reg   = (volatile uint8_t *)0x23, // PINB
+
+  *portc_reg  = (volatile uint8_t *)0x28, // PORTC
+  *ddrc_reg   = (volatile uint8_t *)0x27, // DDRC
+  *pinc_reg   = (volatile uint8_t *)0x26, // PINC
+
+  *portd_reg  = (volatile uint8_t *)0x2B, // PORTD
+  *ddrd_reg   = (volatile uint8_t *)0x2A, // DDRD
+  *pind_reg   = (volatile uint8_t *)0x29; // PIND
