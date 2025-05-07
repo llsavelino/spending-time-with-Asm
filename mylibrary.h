6440a274192a361bad5273e4d17eb9d45c8842a5
Lenilson A. Santos <savelino852@gmail.com>
2025-05-07 09:46:58 -0300

    Atualizar o mylibrary.h

--- mylibrary.h
+++ mylibrary.h
@@ -9,14 +9,15 @@ extern "C"
 #ifdef __cplusplus
 }
 #endif
+// ÷=-=-=÷ <[ B ]> ÷=-=-=÷
 extern volatile uint8_t* portb_reg;
 extern volatile uint8_t*  ddrb_reg;
 extern volatile uint8_t*  pinb_reg;
-
+// ÷=-=-=÷ <[ C ]> ÷=-=-=÷
 extern volatile uint8_t* portc_reg;
 extern volatile uint8_t*  ddrc_reg;
 extern volatile uint8_t*  pinc_reg;
-
+// ÷=-=-=÷ <[ D ]> ÷=-=-=÷
 extern volatile uint8_t* portd_reg;
 extern volatile uint8_t*  ddrd_reg;
 extern volatile uint8_t*  pind_reg;
